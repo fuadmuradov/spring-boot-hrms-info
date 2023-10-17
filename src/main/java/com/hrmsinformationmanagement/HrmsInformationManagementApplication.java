aed3ac3a7f1f607f5ce3ac4ac7c65c09f8047a64
fuadmuradov-em <fuad.muradov@emanat.az>
2023-10-17 17:24:44 +0400

    first commit

---
+++ src/main/java/com/hrmsinformationmanagement/HrmsInformationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hrmsinformationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsInformationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsInformationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrmsinformationmanagement/HrmsInformationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmsinformationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsInformationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
